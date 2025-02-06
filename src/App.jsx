b5eb02379f2f9bb2cdb03ea16881462cb78414a1
Mario Ponce <mario@mpondev.com>
2025-02-06 19:00:39 +0100

    Add Navbar component with hamburger menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Navbar from './components/Navbar';
+
 function App() {
   return (
     <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
-      Full Stack Blog
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="w-full h-16 md:h-20 flex items-center justify-between">
+      <div className="flex items-center gap-4 text-2xl font-bold">
+        <img src="/favicon.svg" className="size-8" alt="Logo" />
+        <span>mponblog</span>
+      </div>
+
+      <div className="md:hidden">
+        <div
+          className="cursor-pointer text-3xl"
+          onClick={() => setIsOpen(prev => !prev)}
+        >
+          {isOpen ? 'X' : '☰'}
+        </div>
+
+        <div
+          className={`w-full h-screen flex flex-col items-center justify-center gap-8 font-medium text-lg absolute top-16 transition-all ease-in-out ${
+            isOpen ? '-right-0' : '-right-full'
+          }`}
+        >
+          <a href="/">Home</a>
+          <a href="/">Trending</a>
+          <a href="/">Most Popular</a>
+          <a href="/">About</a>
+          <a href="/">
+            <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
+              Login
+            </button>
+          </a>
+        </div>
+      </div>
+
+      <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
+        <a href="/">Home</a>
+        <a href="/">Trending</a>
+        <a href="/">Most Popular</a>
+        <a href="/">About</a>
+        <a href="/">
+          <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
+            Login
+          </button>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
