9143a1369dc1fa39b397e90359fae357e7ec6662
Mario Ponce <mario@mpondev.com>
2025-02-10 15:11:18 +0100

    Add Error handling

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import connectDB from './lib/connectDB.js';
 import userRouter from './routes/user.route.js';
 import postRouter from './routes/post.route.js';
 import commentRouter from './routes/comment.route.js';
+import e from 'express';
 
 const app = express();
 const PORT = 3000;
@@ -14,6 +15,15 @@ app.use(express.json());
 app.use('/users', userRouter);
 app.use('/posts', postRouter);
 app.use('/comments', commentRouter);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send({
+    message: err.message || 'Something went wrong!',
+    stack: err.stack,
+    status: err.status || 500,
+  });
+});
 
 app.listen(PORT, () => {
   connectDB();
